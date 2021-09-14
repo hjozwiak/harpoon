c60b66432ad54e5bd6d24473f408448307fd85b3
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-09-14 16:41:44 -0400

    Somewhat more progress.

--- harpoon-atspi/src/lib.rs
+++ harpoon-atspi/src/lib.rs
@@ -1,22 +1,12 @@
 #[macro_use]
+mod rt;
+#[macro_use]
 extern crate glib;
-extern crate glib_sys;
 extern crate ffi;
-
+extern crate glib_sys;
 extern crate libc;
 #[macro_use]
 extern crate bitflags;
 
-
 pub use auto::*;
 mod auto;
-mod rect;
-pub use rect::*;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
