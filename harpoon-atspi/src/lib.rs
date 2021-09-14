c3e56cc7dd348c0a2a921e7d19d74e82b383ff78
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-09-14 13:05:17 -0400

    Starting to work on the safe code for the user. This will take some doing; ironically it's a lot harder than doing the FFI portion.

--- harpoon-atspi/src/lib.rs
+++ harpoon-atspi/src/lib.rs
@@ -1,3 +1,15 @@
+#[macro_use]
+extern crate glib;
+extern crate glib_sys;
+extern crate atspi_sys;
+
+extern crate libc;
+#[macro_use]
+extern crate bitflags;
+
+
+pub use auto::*;
+mod auto;
 #[cfg(test)]
 mod tests {
     #[test]
