2564640fa8b6f782d56313a3a0b3c7aa2d068045
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-09-17 00:45:43 -0400

    Final changes.

---
+++ src/generated/mod.rs
@@ -0,0 +1,3 @@
+//! Generated crates from the at-spi2-core repository.
+pub mod accessible;
+pub mod bus;
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod generated;
-use generated::BusProxy;
+use generated::bus::BusProxy;
 use log::{debug, error, info};
 use simple_log::quick;
 use zbus::Connection;
 fn main() {
-    quick();
+    quick().expect("Error getting the log server setup.");
     debug!("Starting program.");
     let connection = Connection::new_session().expect("Could not connect to bus.");
-    let proxy = BusProxy::new(&connection).expect("Error obtaining proxy object.");
+    let proxy = generated::bus::BusProxy::new(&connection).expect("Error obtaining proxy object.");
     let actual_bus = proxy.get_address();
     match actual_bus {
         Ok(address) => debug!("Found the atspi bus at {}", address),
