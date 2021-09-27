524442153ab0ea419ac213d7d3d19131b971aa06
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-09-27 08:47:19 -0400

    Drop unused things for now.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
+use std::time::Duration;
 mod generated;
-use generated::bus::BusProxy;
-use log::{debug, error, info};
-use simple_log::quick;
-use zbus::Connection;
+use dbus::blocking::Connection;
 fn main() {
-    quick().expect("Error getting the log server setup.");
-    debug!("Starting program.");
-    let connection = Connection::new_session().expect("Could not connect to bus.");
-    let proxy = generated::bus::BusProxy::new(&connection).expect("Error obtaining proxy object.");
-    let actual_bus = proxy.get_address();
-    match actual_bus {
-        Ok(address) => debug!("Found the atspi bus at {}", address),
-        Err(e) => error!("Problem getting the bus. {}", e),
-    }
+    // register on the user bus.
+    let connection = Connection::new_session().expect("Failed to connect to the session bus.");
+    let proxy = connection.with_proxy(
+        "org.a11y.atspi.Bus",
+        "/org/a11y/atspi/bus",
+        Duration::new(5, 0),
+    );
+    use generated::OrgA11yBus;
+    let new_address = proxy.get_address().expect("Failed to get the address.");
+    println!(new_address);
 }
