607fa613071251ffe27955e5540cf11083b3a387
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-10-05 12:43:46 -0400

    Revert to the base application for now to do the actual dbus work in the proper branch.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-use std::time::Duration;
-mod generated;
-use dbus::blocking::Connection;
 fn main() {
-    // register on the user bus.
-    let connection = Connection::new_session().expect("Failed to connect to the session bus.");
-    let proxy = connection.with_proxy(
-        "org.a11y.atspi.Bus",
-        "/org/a11y/atspi/bus",
-        Duration::new(5, 0),
-    );
-    use generated::OrgA11yBus;
-    let new_address = proxy.get_address().expect("Failed to get the address.");
-    println!(new_address);
+    println!("Hello, world!");
 }
